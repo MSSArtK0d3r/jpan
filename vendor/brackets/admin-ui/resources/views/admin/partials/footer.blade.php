22d924d552a351e45d9cb7be7eb35e8029e71f96
etadiguu88 <61055113+etadiguu88@users.noreply.github.com>
2021-03-01 04:42:05 +0800

    v1.0.1

---
+++ vendor/brackets/admin-ui/resources/views/admin/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="app-footer">
+    <div class="container-fluid">
+        <div class="container-xl">
+            <span class="pull-right">&copy2021 JPAN</span>
+        </div>
+    </div>
+</footer>
