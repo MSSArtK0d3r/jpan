ff483c572a3e9827328f317665eee3727e10c6a2
etadiguu88 <61055113+etadiguu88@users.noreply.github.com>
2021-08-19 09:49:46 +0800

    v1.18.1

--- resources/views/users/demography.blade.php
+++ resources/views/users/demography.blade.php
@@ -348,9 +348,9 @@
                      tahun
                      <select name="tempohPerkhidmatanBulan" class="selectBox">
                         @if ( $userData[0]->tempohPerkhidmatanBulan == NULL )
-                        <option value="" selected disabled>Sila pilih</option><option value="0">0</option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option>
+                        <option value="" selected disabled>Sila pilih</option><option value="0">0</option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option>
                         @else
-                           <option value="{{$userData[0]->tempohPerkhidmatanBulan}}">[Pilihan] - {{$userData[0]->tempohPerkhidmatanBulan}}</option><option value="0">0</option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option> 
+                           <option value="{{$userData[0]->tempohPerkhidmatanBulan}}">[Pilihan] - {{$userData[0]->tempohPerkhidmatanBulan}}</option><option value="0">0</option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option> <option value="12">12</option>
                         @endif
                      </select>
                      bulan
