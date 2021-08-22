6c5d05521529905ac3859533884fd8fd9b79f6f2
etadiguu88 <61055113+etadiguu88@users.noreply.github.com>
2021-08-22 11:37:04 +0800

    v1.20.1-testDebug

--- resources/views/components/ratings-components.blade.php
+++ resources/views/components/ratings-components.blade.php
@@ -1,7 +1,10 @@
 <div class="radio">
     @for($i = $initialQuestion; $i <= 10; $i++)
-    <input {{$completedR == 1 ? 'class=sectionDone' : ''}} {{$userData == $i ? 'checked' : ''}} type="radio" label="{{$i}}" id="{{$sectionQuestion.$questionNumber.$i}}" name="{{$sectionQuestion.$questionNumber}}" value="{{$i}}">
+    <span>{{$completedR}}</span>
+    <input  {{$userData == $i ? 'checked' : ''}} type="radio" label="{{$i}}" id="{{$sectionQuestion.$questionNumber.$i}}" name="{{$sectionQuestion.$questionNumber}}" value="{{$i}}">
     
     @endfor
 </div>
+
+{{-- {{$completedR == 1 ? 'class=sectionDone' : ''}} --}}
 
