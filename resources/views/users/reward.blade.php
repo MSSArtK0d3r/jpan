fe04944e8369c0a2dc19ee1cb8bc516a4377fc07
etadiguu88 <61055113+etadiguu88@users.noreply.github.com>
2021-09-01 09:32:51 +0800

    v208 update 3

--- resources/views/users/reward.blade.php
+++ resources/views/users/reward.blade.php
@@ -2,7 +2,7 @@
 <div class="contents eight columns">
     <h5 class="title-center">Maklumat Bayaran Saguhati</h5>
     <p style="text-align: justify">
-        Kumpulan penyelidik merakamkan ucapan ribuan terima kasih atas kesudian Tuan/Puan melengkapkan soal selidik I-KOBT. Sebagai tanda penghargaan, pihak penyelidik akan membayar sagu hati berjumlah RM10.00 akaun bank Tuan/Puan. Justeru, pihak kami memohon jasa baik dan kerjasama daripada pihak Tuan/Puan memberikan maklumat berikut:
+        Kumpulan penyelidik merakamkan ucapan ribuan terima kasih atas kesudian Tuan/Puan melengkapkan soal selidik I-KOBT. Sebagai tanda penghargaan, pihak penyelidik akan membayar sagu hati berjumlah RM10.00 ke akaun bank Tuan/Puan. Justeru, pihak kami memohon jasa baik dan kerjasama daripada pihak Tuan/Puan memberikan maklumat berikut:
     </p>
     <p style="text-align: justify;font-size: 12px;font-style: italic;">
         (Nota: Bayaran sagu hati hanya diberikan kepada 6,122 penjawat awam terawal yang mengisi I-KOBT dan telah mendapat pengesahan daripada pihak JPAN)
