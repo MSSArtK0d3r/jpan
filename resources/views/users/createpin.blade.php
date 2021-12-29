fe359d963a8521290eea3c6a11da0a2187256e5a
etadiguu88 <61055113+etadiguu88@users.noreply.github.com>
2021-12-30 00:29:42 +0800

    v2107

--- resources/views/users/createpin.blade.php
+++ resources/views/users/createpin.blade.php
@@ -2,6 +2,7 @@
 <div class="contents twevle columns">
     <div style="display: flex;flex-wrap: wrap;justify-content: center;" class="row">
         <h3>PIN Sementara anda telah di hantar ke email yang telah berdaftar dengan I-KOBT</h3>
+        <a href="{{route('verifypinpage')}}">Log masuk akaun anda</a>
     </div>
 </div>
 <x-footer />
